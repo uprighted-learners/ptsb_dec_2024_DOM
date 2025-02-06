92ecf8ec6d57e635f54d421547f440e4d872fed7
u4ik <insighteuphoric@gmail.com>
2025-02-05 20:40:37 -0500

    adding form events, and query params

--- form/index.js
+++ form/index.js
@@ -2,7 +2,7 @@ let form = document.querySelector("form");
 if (form){
     form.addEventListener("submit", (e) => {
         //? Preventing the page from refreshing
-        e.preventDefault()
+        // e.preventDefault()
         // console.log(e.target);
 
         const formElement = e.target
@@ -14,5 +14,75 @@ if (form){
         console.log({nameValue});
         console.log({emailValue});
         console.log({foodValue});
+        //! Will not allow query param values to carry over
+        // formElement.reset()
     })
+}
+
+// Creating our own query string here.
+let paramString = "?name=foo&email=test@hotmail.com"
+// Another approach would be to obtain the query string, using document.location.search
+console.log(document.location.search); // Use this to slice out the query string found in the address bar
+let searchParams = new URLSearchParams(paramString) 
+
+// console.log(searchParams);
+
+// Looking at all the keys in query params once we use the URLSearchParams Interface
+searchParams.keys().forEach((i) => {
+    // console.log(i);
+})
+
+// Looking at all the values ...
+searchParams.values().forEach(i => {
+    // console.log(i);
+})
+
+// Check if query string has a particular key name
+// console.log(searchParams.has("name")); //? true, key exists
+
+// Check if a value sits behind a key
+// console.log(searchParams.get("email"));
+
+searchParams.entries().forEach((i) => {
+    // console.log(i);
+})
+
+
+
+let ul = document.querySelector("ul")
+
+if (ul){
+    console.log(ul);
+    let currentPageParams = new URLSearchParams(document.location.search)
+
+    currentPageParams.values().forEach((val) => {
+        let listItem = document.createElement("li")
+        listItem.textContent = val
+        ul.appendChild(listItem)
+    })
+}
+
+let myObjValues = {
+    name: "Bugs Bunny",
+    email: "bunny@bugs.com",
+    food: "carrots"
+}
+
+let myParams = new URLSearchParams(myObjValues)
+
+console.log(myParams.get("email")); // bunny@bugs.com
+
+
+let qButton = document.querySelector(".qParams")
+if(qButton){
+    qButton.addEventListener("click", () => {
+        myParams.set("name", "Timmy")
+        document.location.search = myParams
+    })
+}
+
+let myLink = document.querySelector("a")
+console.log(myLink);
+if(myLink) {
+    myLink.href = "./index2.html" + document.location.search
 }
